f8306e07e25018181e7db5a9eecabfd3c23258fc
Dallon Feldner <dallon@sibipro.com>
2022-12-14 12:35:02 -0600

    Day 01 Part One

--- src/load_algorithm.rs
+++ src/load_algorithm.rs
@@ -5,7 +5,7 @@ use crate::lua::draw_runtime::DrawRuntime;
 use crate::prelude::*;
 use crate::puzzles;
 
-type ThreadFunc = Box<dyn (Fn(&Box<dyn ReportProgress>) -> String) + Send>;
+pub type ThreadFunc = Box<dyn (Fn(&Box<dyn ReportProgress>) -> Result<String>) + Send>;
 pub struct Algorithm {
     pub draw_runtime: DrawRuntime,
     pub thread_func: ThreadFunc,
@@ -13,10 +13,12 @@ pub struct Algorithm {
 
 pub fn load(day: &str, part: &str) -> Result<Algorithm> {
     let thread_func: ThreadFunc = match (day, part) {
-        ("test_algo", "part_one") => Box::new(|_| puzzles::test_algo::part_one().to_string()),
-        ("test_algo", "part_two") => Box::new(|rp| puzzles::test_algo::part_two(rp).to_string()),
-        ("day01", "part_one") => Box::new(|_| puzzles::day01::part_one().to_string()),
-        ("day01", "part_two") => Box::new(|_| puzzles::day01::part_two().to_string()),
+        ("test_algo", "part_one") => Box::new(|_| Ok(puzzles::test_algo::part_one().to_string())),
+        ("test_algo", "part_two") => {
+            Box::new(|rp| Ok(puzzles::test_algo::part_two(rp).to_string()))
+        }
+        ("day01", "part_one") => Box::new(|_| puzzles::day01::part_one().map(|it| it.to_string())),
+        ("day01", "part_two") => Box::new(|_| puzzles::day01::part_two().map(|it| it.to_string())),
         (_, _) => bail!("Couldn't find {day} {part}"),
     };
 
