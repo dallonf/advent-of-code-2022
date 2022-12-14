a4cafbf19655888caccc9ab21b1d1b3722bdbad8
Dallon Feldner <dallon@sibipro.com>
2022-12-14 16:43:13 -0600

    Day Two Part Two

--- src/puzzles/day02/mod.rs
+++ src/puzzles/day02/mod.rs
@@ -19,6 +19,24 @@ enum RPSMove {
     Scissors,
 }
 
+impl RPSMove {
+    fn beats(&self) -> Self {
+        match self {
+            Self::Rock => Self::Scissors,
+            Self::Paper => Self::Rock,
+            Self::Scissors => Self::Paper,
+        }
+    }
+
+    fn beaten_by(&self) -> Self {
+        match self {
+            Self::Rock => Self::Paper,
+            Self::Paper => Self::Scissors,
+            Self::Scissors => Self::Rock,
+        }
+    }
+}
+
 impl FromStr for Strategy {
     type Err = anyhow::Error;
 
@@ -68,20 +86,18 @@ enum Winner {
 }
 
 fn get_winner(left: RPSMove, right: RPSMove) -> Winner {
-    match (left, right) {
-        (RPSMove::Rock, RPSMove::Scissors) => return Winner::Left,
-        (RPSMove::Paper, RPSMove::Rock) => return Winner::Left,
-        (RPSMove::Scissors, RPSMove::Paper) => return Winner::Left,
-        (_, _) => {}
-    }
     if left == right {
-        return Winner::Tie;
+        Winner::Tie
+    } else if left.beats() == right {
+        Winner::Left
+    } else if left.beaten_by() == right {
+        Winner::Right
+    } else {
+        panic!("This shouldn't be possible")
     }
-    // unproven assumption: if it's not a win for left, and it's not a tie, it has to be a win for right
-    return Winner::Right;
 }
 
-fn get_total_score_for_strategy(strategy: Strategy) -> Result<i64> {
+fn get_total_score_for_strategy(strategy: &Strategy) -> Result<i64> {
     let outcomes: Result<Vec<i64>> = strategy
         .0
         .iter()
@@ -109,11 +125,38 @@ fn get_total_score_for_strategy(strategy: Strategy) -> Result<i64> {
 
 pub fn part_one() -> Result<i64> {
     let strategy = include_str!("./puzzle_input.txt").parse()?;
-    get_total_score_for_strategy(strategy)
+    get_total_score_for_strategy(&strategy)
+}
+
+fn get_total_score_for_smarter_strategy(strategy: &Strategy) -> Result<i64> {
+    let outcomes: Result<Vec<i64>> = strategy
+        .0
+        .iter()
+        .map(|entry| {
+            let opponent_move = match entry.0 {
+                'A' => RPSMove::Rock,
+                'B' => RPSMove::Paper,
+                'C' => RPSMove::Scissors,
+                other => bail!("Unexpected opponent move: {other}"),
+            };
+
+            let your_move = match entry.1 {
+                'X' => opponent_move.beats(),
+                'Y' => opponent_move,
+                'Z' => opponent_move.beaten_by(),
+                other => bail!("Unexpected player move: {other}"),
+            };
+
+            Ok(round_score(your_move, opponent_move))
+        })
+        .collect();
+
+    Ok(outcomes?.into_iter().sum())
 }
 
 pub fn part_two() -> Result<i64> {
-    todo!()
+    let strategy = include_str!("./puzzle_input.txt").parse()?;
+    get_total_score_for_smarter_strategy(&strategy)
 }
 
 #[cfg(test)]
@@ -143,5 +186,17 @@ mod test {
     #[test]
     fn part_one_answer() {
         insta::assert_display_snapshot!(part_one().unwrap(), @"15422");
+    }
+
+    #[test]
+    fn smarter_strategy() {
+        let strategy = Strategy::from_str(SAMPLE_INPUT).unwrap();
+        let result = get_total_score_for_smarter_strategy(&strategy).unwrap();
+        assert_eq!(result, 12);
+    }
+
+    #[test]
+    fn part_two_answer() {
+        assert_eq!(part_two().unwrap(), 15442);
     }
 }
