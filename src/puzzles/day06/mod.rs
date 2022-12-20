34cae0b69443efdf0bcc741fcd0790facc7915cf
Dallon Feldner <dallon.feldner@gmail.com>
2022-12-19 21:15:52 -0600

    Day 6 Part Two

--- src/puzzles/day06/mod.rs
+++ src/puzzles/day06/mod.rs
@@ -1,3 +1,5 @@
+// Day 6: Tuning Trouble
+
 use crate::prelude::*;
 
 pub fn part_one() -> Result<usize> {
@@ -5,23 +7,41 @@ pub fn part_one() -> Result<usize> {
         .ok_or_else(|| anyhow!("No start-of-packet marker detected."))
 }
 
-pub fn part_two() -> Result<String> {
-    todo!()
+pub fn part_two() -> Result<usize> {
+    first_unique_slice(include_str!("./puzzle_input.txt"), 14)
+        .ok_or_else(|| anyhow!("No start-of-message marker detected."))
 }
 
-const MARKER_LENGTH: usize = 4;
 fn first_new_packet_marker(input: &str) -> Option<usize> {
+    first_unique_slice(input, 4)
+}
+
+fn first_unique_slice(input: &str, slice_length: usize) -> Option<usize> {
     let char_list: Vec<char> = input.chars().collect();
-    for (i, slice) in char_list.windows(MARKER_LENGTH).enumerate() {
-        if is_new_packet_marker(slice) {
-            return Some(i + MARKER_LENGTH);
+    let previous_slice_length = slice_length - 1;
+    let mut last_duplicate: Option<usize> = None;
+    for (i, current_char) in char_list.iter().enumerate() {
+        let (slice_start, is_warm) = if i > previous_slice_length {
+            (i - previous_slice_length, true)
+        } else {
+            (0, false)
+        };
+        let previous_slice = &char_list[slice_start..i];
+        for (back_i, back_char) in previous_slice.iter().enumerate().rev() {
+            if back_char == current_char {
+                let found_duplicate = slice_start + back_i;
+                last_duplicate = last_duplicate.map_or(found_duplicate, |prev_duplicate| {
+                    found_duplicate.max(prev_duplicate)
+                }).pipe(Some);
+                break;
+            }
+        }
+        let has_duplicate = last_duplicate.map(|it| it >= slice_start).unwrap_or(false);
+        if !has_duplicate && is_warm {
+            return Some(i + 1);
         }
     }
     None
-}
-
-fn is_new_packet_marker(slice: &[char]) -> bool {
-    slice.iter().duplicates().next().is_none()
 }
 
 #[cfg(test)]
@@ -55,5 +75,10 @@ mod test {
     #[test]
     fn part_one_answer() {
         assert_eq!(part_one().unwrap(), 1093);
+    }
+
+    #[test]
+    fn part_two_answer() {
+        assert_eq!(part_two().unwrap(), 3534);
     }
 }
